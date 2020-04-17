1991e4376c2243b18d8d2eef3ef41b0ab9cd13fa
Andri <andricampagnaro@gmail.com>
2020-04-16 22:59:31 -0300

    Brython adicional

--- brython/login_form.py
+++ brython/login_form.py
@@ -1,15 +1,18 @@
 from browser import document
-from browser.html import INPUT, LABEL, BR, FORM, H1
+from browser.html import INPUT, LABEL, BR, FORM, H1, H2
+
+def cabecalho():
+    document <= H1('Página escrita em Brython')
 
 def login_form():
-    form = FORM(id="meu-forme", action="#", method="GET", target="blank")
+    form = FORM(Class='form login-form', id="meu-forme", action="#", method="GET", target="blank")
 
-    form <= H1('Meu formulário em Brython')
+    form <= H2('Meu formulário')
     form <= LABEL('seu nome', For='nome')
-    form <= INPUT(id='nome', name='nome')
+    form <= INPUT(id='nome', name='nome', required=True)
     form <= BR()
     form <= LABEL('sua senha', For='senha')
-    form <= INPUT(id='senha', type='password', name='senha')
+    form <= INPUT(id='senha', type='password', name='senha', required=True)
     form <= BR()
     form <= INPUT(id='submit', type='submit')
 
